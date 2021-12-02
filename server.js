f5f48dcbf74b3f510e017bfd5d5f58de33566fef
RefalaXia <ruoting@live.unc.edu>
2021-12-02 14:54:06 -0500

    try6

--- server.js
+++ server.js
@@ -23,10 +23,15 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-app.post( "/app/new", (req, res) => {
+app.post("/app/new", (req, res) => {
+	var data = {
+		user: req.body.user,
+		pass: req.body.pass,
+		id: req.params.id
+	}
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
 	const info = stmt.run(req.body.user, md5(req.body.pass));
-	res.status(200).json({"message": info.changes+ " record created: ID " + info.lastInsertRowid + " (201)"}
+	res.status(201).json({"message": info.changes+ " record created: ID " + info.lastInsertRowid + " (201)"}
 	);
 });
 
